76a7018e8b9035f3b5ab44ec51e7acdc41cd1682
Jaber Khan <md.jaberkhan66@gmail.com>
2025-06-08 10:58:46 +0600

    Yii-Learning-Concepts

--- 1.Yii-2-Basic-Learning/controllers/ArticleController.php
+++ 1.Yii-2-Basic-Learning/controllers/ArticleController.php
@@ -2,11 +2,49 @@
 
 namespace app\controllers;
 
+use yii\helpers\VarDumper;
+
 class ArticleController extends \yii\web\Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function beforeAction($action){
+        if($action->id == 'index'){
+            echo '<pre>';
+            // var_dump($action); // if wanted to called the overall configuration called actions
+            var_dump($action->id); // accessing requested actionId
+            // var_dump($action->controller); // accessing its controller informaton
+            var_dump($action->behaviors());
+            echo '</pre>';
+        }
+
+        // if parent action called retunr parent::beforeAction() called the server info in console
+        if(!parent::beforeAction($action)){
+            return parent::beforeAction($action);
+        } else{
+            return false;
+        }
+    }
+
+    public function actionView($id){
+        return $this->render('view',['model'=>$this->findModel($id)]);
+    }
+
+    // show the passed query string 
+    public function actionParams($query_strings = null){
+        
+        if(is_array($query_strings)){
+            if($query_strings != null){
+                echo "<pre>";
+                var_dump('Query strings are: ', $query_strings);
+                echo "</pre>";
+            } else{
+                echo VarDumper::dump($query_strings);
+            }
+        }
     }
 
 }
